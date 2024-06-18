51cce1bb30b1ef9ca5aeab10bfa8f9fabd0226dc
Kenneth Azumara <kenazumara@gmail.com>
2024-06-18 14:58:04 +0100

    Added comments Mske better code

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,6 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Address = require('../models/addressModel');
 
-// const orderModel = require('../models/orderModel');
-
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
@@ -101,105 +99,177 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllUsers = factory.getAll(User);
-exports.getUser = factory.getOne(User);
+exports.getUser = factory.getOne(User, { path: 'addresses' });
 exports.removeUser = factory.deleteOne(User);
 exports.updateUser = factory.updateOne(User);
 
 // USER ADDRESS
 exports.createAddress = asyncHandler(async (req, res) => {
   const userId = req.user.id;
-  const address = new Address({
+  const address = {
     street: req.body.street,
     city: req.body.city,
     state: req.body.state,
     zip: req.body.zip,
     country: req.body.country,
     user: userId, // Associate the address with the user
-  });
+  };
 
-  await address.save();
+  const userAddress = await Address.create(address);
   res.status(201).json({
     status: 'success',
     data: {
-      address,
+      data: userAddress,
     },
   });
 });
 
+exports.getAddress = factory.getAll(Address);
+exports.deleteAddress = factory.deleteOne(Address);
+
 // USER CARTS
-exports.createUserCart = asyncHandler(async (req, res) => {
+
+exports.addCart = asyncHandler(async (req, res) => {
   const { cart } = req.body;
-  // const products = [];
+
+  // Check if cart is defined and is an array
+  if (!cart || !Array.isArray(cart)) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Cart must be an array and cannot be empty',
+    });
+  }
 
   const user = await User.findById(req.user.id);
 
-  // check if user already have product in cart
-  const alreadyExistCart = await Cart.findOne({ orderby: user._id });
-  if (alreadyExistCart) {
-    await Cart.deleteOne({ _id: alreadyExistCart._id });
+  // Find the user's cart
+  let existingCart = await Cart.findOne({ orderby: user.id });
+
+  if (!existingCart) {
+    // If no cart exists, create a new one
+    existingCart = new Cart({ orderby: user.id, items: [] });
   }
 
   const getCartProductPromises = cart.map(async (cartItem) => {
-    const product = await Product.findById(cartItem._id);
+    const product = await Product.findById(cartItem.id);
 
     const selectedColor = product.color.find((colorItem) =>
       colorItem.color.toLowerCase().includes(cartItem.color.toLowerCase()),
     );
 
-    // Calculate price based on selected color
-    // const price = selectedColor.discountPrice;
-    // console.log(selectedColor);
-
     return {
-      price: selectedColor.discountPrice,
-      quantity: cartItem.quantity,
-      product: cartItem._id,
+      product: cartItem.id,
       color: cartItem.color,
       name: product.name,
+      description: product.description,
       image: selectedColor.colorImage,
+      price: selectedColor.price,
+      discountPrice: selectedColor.discountPrice,
+      quantity: cartItem.quantity,
+      maxQuantity: selectedColor.quantity,
     };
   });
 
   const products = await Promise.all(getCartProductPromises);
-  let cartTotal = 0;
-  for (let i = 0; i < products.length; i++) {
-    cartTotal += products[i].price * products[i].quantity;
-  }
 
-  const newCart = await new Cart({
-    products,
-    cartTotal,
-    orderby: user.id,
-  }).save();
+  products.forEach((newItem) => {
+    const existingItemIndex = existingCart.items.findIndex(
+      (item) =>
+        item.product.toString() === newItem.product &&
+        item.color === newItem.color,
+    );
 
-  // await newCart.populate('orderby', 'name -_id');
+    if (existingItemIndex > -1) {
+      existingCart.items[existingItemIndex].quantity += newItem.quantity;
+    } else {
+      existingCart.items.push(newItem);
+    }
+  });
+
+  existingCart.cartSubtotal = existingCart.items
+    .reduce((total, item) => total + item.discountPrice * item.quantity, 0)
+    .toFixed(2);
+
+  existingCart.totalQuantityOrdered = existingCart.items.reduce(
+    (total, item) => total + item.quantity,
+    0,
+  );
+
+  await existingCart.save();
 
   res.status(200).json({
     status: 'success',
-    data: newCart,
+    data: existingCart,
   });
 });
 
-exports.getUserCart = asyncHandler(async (req, res, next) => {
-  const cart = await Cart.findOne({ orderby: req.user.id }).populate(
-    'products.product',
-    'name description tag',
+exports.updateCart = asyncHandler(async (req, res, next) => {
+  const { itemId, newQuantity, totalAmt } = req.body;
+
+  const cart = await Cart.findOne({ orderby: req.user.id });
+  if (!cart) {
+    return next(new AppError('No cart found for this user', 404));
+  }
+
+  const itemIndex = cart.items.findIndex(
+    (item) => item._id.toString() === itemId,
   );
-  console.log(cart);
-  if (!cart) return next(new AppError('No cart Found', 404));
+
+  if (itemIndex === -1) {
+    return next(new AppError('Item(s) not found in cart', 404));
+  }
+
+  if (newQuantity <= 0) {
+    cart.items.splice(itemIndex, 1);
+    // Remove item if quantity is zero or less
+  } else {
+    cart.items[itemIndex].quantity = newQuantity; // Update quantity
+  }
+
+  if (!totalAmt || totalAmt === 0) {
+    cart.deliveryFee = 0;
+  } else {
+    cart.deliveryFee = totalAmt.toFixed(2);
+  }
+
+  cart.cartSubtotal = cart.items
+    .reduce((total, item) => total + item.discountPrice * item.quantity, 0)
+    .toFixed(2);
+
+  cart.totalQuantityOrdered = cart.items.reduce(
+    (total, item) => total + item.quantity,
+    0,
+  );
+  await cart.populate('orderby');
+
+  await cart.save();
+
   res.status(200).json({
     status: 'success',
     data: cart,
   });
 });
 
+exports.getUserCart = asyncHandler(async (req, res, next) => {
+  const { totalAmt } = req.body;
+  const cart = await Cart.findOne({ orderby: req.user.id });
+  if (!cart || cart.items.length === 0) {
+    return next(new AppError('No item(s) found in cart', 404));
+  }
+
+  cart.deliveryFee = totalAmt || 0;
+  await cart.save();
+
+  res.status(200).json({
+    results: cart.items.length,
+    status: 'success',
+    data: cart,
+  });
+});
+
 exports.emptyCart = asyncHandler(async (req, res) => {
-  const cart = await Cart.findOneAndDelete({ orderby: req.user.id });
-  console.log(cart);
+  await Cart.findOneAndDelete({ orderby: req.user.id });
   res.status(200).json({
     status: 'success',
   });
 });
-
-exports.getAddress = factory.getAll(Address);
-exports.deleteAddress = factory.deleteOne(Address);
