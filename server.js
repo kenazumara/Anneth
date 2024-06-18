e708a5023d13c6c6077affc1bfe0a2644365c1d5
Kenneth Azumara <kenazumara@gmail.com>
2024-06-18 15:50:57 +0100

    Added meaningful comments, better code

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-// UNHANDLED SYNCHRONOUS ERRORS
+// Load environment variables from config file
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+// Handle synchronous errors
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
+// Connect to MongoDB
 mongoose
   .connect(DB)
   .then(() => console.log('DB connected successfully'))
   .catch((err) => console.log('DB not connected', err));
 
-// console.log(app.get('env'));
-
+// Start the server
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// UNHANDLED ASYNCHRONOUS ERRORS
+// Handle asynchronous errors
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 🔥 Shutting down...');
   console.log(err.name, err.message);
